27a51cdb2b47643e0484fd0726e3505d68e1544e
prabhakarKandel69 <pkandel6969@gmail.com>
2025-07-02 11:33:04 +0545

    Added for ratings and distance

--- recommend_algo.py
+++ recommend_algo.py
@@ -7,25 +7,27 @@ hospitals = [
     {
         "name":"Mero hospital",
         "services":["CT scan","ECG","Full body check","Blood test"],
-        "ratings":13,
+        "ratings":4,
         "coordinates":[27.6664,85.268803]
     },
     {
         "name":"Tero hospital",
         "services":["ECG","Blood test"],
-        "ratings":12,
+        "ratings":3,
         "coordinates":[27.6634,85.26303]
     },
     {
         "name":"Hamro hospital",
         "services":["Blood test"],
-        "ratings":10,
+        "ratings":2,
         "coordinates":[27.6614,85.268103]
     }
 ]
 
 
 def recommend(service_requested,coordinates):
+
+    max_distance = 20
     return_list = []
     for hospital in hospitals:
         if service_requested in hospital["services"]:
@@ -33,14 +35,16 @@ def recommend(service_requested,coordinates):
     
     for hospital in return_list:
         distance = haversine.haversine(coordinates[0],coordinates[1],hospital["coordinates"][0],hospital["coordinates"][1])
-        hospital["distance"] = distance
-    
-    return_list.sort(key=lambda x: x["distance"])
+        norm_rating = hospital["ratings"] / 5.0
+        norm_distance = min(distance/ max_distance,1.0)
 
-
+        score = (0.6*norm_distance) + (0.4*norm_rating)
+        hospital["score"] = score
     
+    return_list.sort(key=lambda x: x["score"])
     return return_list
 
+    
 
 print(recommend("ECG",[26.310,23.449]))
 
